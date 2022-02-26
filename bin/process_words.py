2089829e25a35dcdd52e6b5fa4d54079247e5635
William Gollinger <wgolling@gmail.com>
2022-02-25 20:39:27 -0500

    factor out line processing helper function

--- bin/process_words.py
+++ bin/process_words.py
@@ -5,18 +5,25 @@ def process_words(raw_answers_path):
     with open(raw_answers_path) as file:
         file.readline() # First line is just headings
         line = file.readline()
-        steps = 0
-        while line and steps < 200:
-            line_parts = line.split('\t')
-            date_parts = list(map(int, line_parts[0].split("/")))
-            date_obj = date(date_parts[2],date_parts[0],date_parts[1])
-            game_number = int(line_parts[1])
-            word = line_parts[2].strip()
+        while line:
+            date_obj,game_number,word = _process_line(line)
             print(date_obj)
             print(game_number)
-            print(word)
             line = file.readline()
-            steps += 1
+
+def _process_line(line):
+    '''
+    line is of the form 'mm-dd-yyyy\t{game_number}\t{word}'
+    returns the date as a datetime.date object,
+            the game number as an integer,
+            the word as a string
+    '''
+    line_parts = line.split('\t')
+    date_parts = list(map(int, line_parts[0].split("/")))
+    date_obj = date(date_parts[2],date_parts[0],date_parts[1])
+    game_number = int(line_parts[1])
+    word = line_parts[2].strip()
+    return date_obj,game_number,word
 
 if __name__ == "__main__":
     raw_answers_path = Path('.') / 'data' / 'raw_answers.txt'
