05465fb3f4d62ff234418680e8b8380e884a5703
William Gollinger <wgolling@gmail.com>
2022-02-25 22:23:44 -0500

    formatting

--- bin/process_words.py
+++ bin/process_words.py
@@ -2,32 +2,32 @@ from pathlib import Path
 from datetime import datetime
 import json
 
-def process_words(raw_answers_path):
+def process_words(raw_answers_path, processed_words_path):
     words_by_date = dict()
     with open(raw_answers_path) as file:
-        file.readline() # First line is just headings
+        file.readline()                                                       # First line is just headings.
         line = file.readline()
         while line:
-            date_string,game_number,word = _process_line(line)
+            date_string,_,word = _process_line(line)
             words_by_date[date_string] = word
             line = file.readline()
-    processed_words_path = Path('.') / 'data' / 'processed_words.py'
     with open(processed_words_path, 'w') as file:
-        json.dump(words_by_date, file, default=str)
+        json.dump(words_by_date, file)
 
 def _process_line(line):
     '''
-    line is of the form 'mm/dd/yyyy\t{game_number}\t{word}'
-    returns the date as a datetime.date object,
-            the game number as an integer,
-            the word as a string
+    Assumes line is of the form f'mm/dd/yyyy\t{game_number}\t{word}'
+    Returns strings representing the date,
+                                 the game,
+                                 the word.
     '''
     line_parts = line.split('\t')
-    date_string = datetime.strptime(line_parts[0], '%m/%d/%Y').isoformat()
-    game_number = int(line_parts[1])
+    date_string = datetime.strptime(line_parts[0], '%m/%d/%Y').isoformat()    # Convert datestring to ISO format.
+    game_number = line_parts[1]
     word = line_parts[2].strip()
     return date_string,game_number,word
 
 if __name__ == "__main__":
-    raw_answers_path = Path('.') / 'data' / 'raw_answers.txt'
-    process_words(raw_answers_path)
+    raw_answers_path        = Path('.') / 'data' / 'raw_answers.txt'
+    processed_words_path    = Path('.') / 'data' / 'processed_words.py'
+    process_words(raw_answers_path, processed_words_path)
