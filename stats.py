37a1678800648cb1c983ff38f177aef6b465b6e6
William Gollinger <wgolling@deleto.deleto>
2022-06-07 22:11:05 -0400

    add comments

--- stats.py
+++ stats.py
@@ -2,14 +2,16 @@ import lib.file_io as file_io
 from collections import defaultdict
 from string import ascii_lowercase
 
+# Initialize and filter words dict.
 data = file_io.load_inverted_words()
 max_date = "2027-10-21"
 data = dict(filter(lambda elem: elem[1] < max_date, data.items()))
-word_count = len(data)
 
+# Initialize stats dict.
 int_dict_factory = lambda: defaultdict(int)
 stats = defaultdict(int_dict_factory)
 
+# Populate stats dict.
 for word in data.keys():
     freq = defaultdict(int)
     for char in word:
@@ -18,7 +20,9 @@ for word in data.keys():
         c = char.lower()
         stats[c][freq[c]] += 1
 
+# Compute occurrence percentages and write to a string.
 stats_string = ""
+word_count = len(data)
 for c in ascii_lowercase:
     char_stats = stats[c]
     char_stat_strings = []
@@ -34,7 +38,7 @@ for c in ascii_lowercase:
         char_string = char_string + "   " + p    
     stats_string += char_string + "\n"
 
+# Save stats string.
 stats_file_path = file_io._LETTER_STATS
-
 with open(stats_file_path, 'w') as f:
     f.write(stats_string)
